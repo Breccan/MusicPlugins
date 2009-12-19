45e78a774a46ae10ab4f760a82bfef51030f2878
Breccan <breccan@breccan-laptop.(none)>
2009-12-20 11:23:36 +1300

    signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,6 @@ class User < ActiveRecord::Base
     acts_as_authentic do |c|
     end
 
-    def active?
-      active
-    end
-
     def deliver_password_reset_instructions!
       reset_perishable_token!  
       Notifier.deliver_password_reset_instructions(self)  
