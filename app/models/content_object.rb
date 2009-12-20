47fa0a7528af1cbe28194ac1b8ddbecd0656783e
Will Marshall <willrj.marshall@gmail.com>
2009-12-20 14:38:51 +1300

    Adding validation and tests for content object

--- app/models/content_object.rb
+++ app/models/content_object.rb
@@ -1,7 +1,10 @@
 class ContentObject < ActiveRecord::Base
+  URL_REGEXP = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
   belongs_to :user
   belongs_to :category
   has_many :tags
   
-  validates_presence_of :name
+  validates_presence_of :name, :description, :user_id, :category_id
+  validates_format_of :video_url, :homepage, :with => URL_REGEXP, :allow_nil => true
+  
 end
