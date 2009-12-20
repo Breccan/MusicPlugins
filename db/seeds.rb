acc3307e166d8c6241c30be5a5fad918d93ff7ba
Breccan <breccan@breccan-laptop.(none)>
2009-12-20 17:08:22 +1300

    more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
-[["Ableton Project", "Project folders and .als files for Ableton Live"]].each do |category|
-  Category.create(:name => category[0], :description => category[1])
+[["Ableton Project", "Project folders and .als files for Ableton Live"], ["Max Patches", "Patches for Max/MSP"]].each do |category|
+  c = Category.find_or_create_by_name(category[0])
+  c.description = category[1]
+  c.save
 end
