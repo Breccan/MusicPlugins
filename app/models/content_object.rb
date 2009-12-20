ea7d119a3f2bbfc8b01cdd76e77a1ed65adbfa0b
Breccan <breccan@breccan-laptop.(none)>
2009-12-20 16:37:51 +1300

    taggable

--- app/models/content_object.rb
+++ app/models/content_object.rb
@@ -5,6 +5,7 @@ class ContentObject < ActiveRecord::Base
   has_many :tags
   has_attached_file :image, :style => { :thumb => {"125x100#", :png}}
   has_attached_file :content 
+  acts_as_taggable
 
   validates_presence_of :name, :description, :user_id, :category_id
   validates_format_of :video_url, :homepage, :with => URL_REGEXP, :allow_blank => true
