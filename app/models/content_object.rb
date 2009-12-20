f3b50fdf65d1f12364b5da0df86a66840e81a7ac
Will Marshall <willrj.marshall@gmail.com>
2009-12-20 14:50:27 +1300

    Adding reset.css

--- app/models/content_object.rb
+++ app/models/content_object.rb
@@ -3,8 +3,8 @@ class ContentObject < ActiveRecord::Base
   belongs_to :user
   belongs_to :category
   has_many :tags
-  
+
   validates_presence_of :name, :description, :user_id, :category_id
   validates_format_of :video_url, :homepage, :with => URL_REGEXP, :allow_nil => true
-  
+
 end
